5dcc142eea31962967c6d48c810315c366e0d10b
Peter Clarke <petermclarke@gmail.com>
2014-03-03 22:32:47 +0000

    Simple HI on the matrix

--- main.c
+++ main.c
@@ -3,63 +3,67 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define PIN_DATA   PB0 // DS
-#define PIN_CLOCK  PB1 // SH_CP
-#define PIN_LATCH  PB2 // ST_CP
+
+#define PIN_DATA   PB3 // DS - 11
+#define PIN_CLOCK  PB5 // SH_CP - SCK - 13
+#define PIN_LATCH  PB4 // ST_CP - 12
+#define PIN_OE     PC3 // Output Enable - 17
 
 // From Android.h
 #define LSBFIRST 0
 #define MSBFIRST 1
 
+#define T1 2 // timeout value for the multiplex (mSec)
+
 /********************************************************************************
 Function Prototypes
 ********************************************************************************/
-void sandpit();
-void countTime();
+
+void initialize(void); //all the usual mcu stuff
+void setFrame(uint8_t pattern[8]);
+void outputRow(uint8_t red, uint8_t blue, uint8_t green);
 void shiftOut(uint8_t bitOrder, uint8_t val);
 
-void shiftRow(char out);
-void draw();
-unsigned long millis();
+
 
 /********************************************************************************
 Global Variables
 ********************************************************************************/
-char cylonDirection; // the current direction the cylon sweep is moving.
-char shift; // the data to be shifted
-char anodes = 0b00000001;
-char cathodes_a; //the first byte of cathodes
-char pattern_index; // which pattern is current
-unsigned long lastSecond;
-unsigned long prev_millis;
+uint8_t anodes = 0b11111110; // low = ON with pnp
+uint8_t current_row; // Which row of the frame is currently being shown via the multiplexing.
+uint8_t current_frame[8]; // The current frame being displayed
+unsigned long current_frame_duration = 2000; // millis to show the current frame.
+unsigned long current_frame_start; // When the current frame was first shown.
 
-static const char patterns[] = {
-	0b01111111,
-	0b10111111,
-    0b11011111,
-    0b10111111,
-    0b01111111,
-    0b10111111,
-    0b11011111,
-    0b10111111,
+uint8_t current_letter; // tmp: the index of font to show
+
+uint8_t font[2][8] = {
+  {0xfe,0xfe,0x10,0x10,0xfe,0xfe,0x00,0x00}, // H
+  {0x00,0x82,0xfe,0xfe,0x82,0x00,0x00,0x00}, // I
 };
 
-volatile char timo_ovf_register; // snapshot of the timer cycle position 0b0100 = 1ms
-volatile unsigned long timo_millis;
+volatile unsigned int time1;
+volatile unsigned int frame_time;
 
 /********************************************************************************
 Interupt Routines
 ********************************************************************************/
 
-// timer overflow occur every 0.256 ms
-ISR(TIM0_OVF_vect) {
-	// when timo_ovf_register reaches 4 then 1ms has passed (near enough)
-	timo_ovf_register++;
-	if (timo_ovf_register == 4) {
-		timo_millis++;
-		// reset to 0
-		timo_ovf_register = 0;
-	}
+//timer 0 compare ISR
+ISR (TIMER0_COMPA_vect)
+{
+	// Called every millis
+
+	// 75 cycles in
+	// 32 cycles save cpu state
+	// 8 cycles overhead
+	// 32 cycles resume cpu state
+	// 75 cycles out
+
+	// Decrement the time if not already zero
+    if (time1 > 0)  --time1;
+    if (frame_time > 0)  --frame_time;
+
 }
 
 
@@ -69,63 +73,43 @@ Main
 int
 main (void)
 {
-    DDRB = 0xFF; // set all to output
+
+	DDRC = 0xFF; // set all to output
+	PORTC |= (1 << PIN_OE); // Output enable high so the display is off
+
+
+	DDRB = 0xFF; // set all to output
 	PORTB = 0; // all off
 
-	//PORTB |= _BV(PB4); // turn the counter debug LED on
-
-	// Cathode 0 = ON, 1 = OFF
-	// RGB RGB RG
-	//cathodes_a = 0b01101111;
-	//cathodes_a = 0; // all ON
-	cathodes_a = 0b11011011;
-	//cathodes_a = 0b00011111;
+	initialize();
 
 
+	setFrame(font[current_letter]);
 
-	// 8 bit timer in mormal mode (attiny85 datasheet 11.7.1)
-	// 1 mhz F_CPU
-	// prescaler 1 (its a slow clock at 1mhz)
-	// 1 / (1000000 / 1 / 256) = 0.000256 = 0.256ms
-	// CS00 sets the prescaler to 1 (attiny85 datasheet 11.9.3)
+	// Now enable the display by setting OE low.
+	PORTC &= ~(1 << PIN_OE);
 
-
-	// if running at 8mhz...
-	// 1 / (8000000 / 8 / 256) = 0.000256 = 0.256ms
-	// CS01 sets the prescaler to 8 (attiny85 datasheet 11.9.3)
-	// prescale timer0 to 1/8th the clock rate
-	// overflow timer0 every 0.256 ms
-
-	TCCR0B |= (1<<CS00);
-	// enable timer overflow interrupt
-	TIMSK  |= 1<<TOIE0;
-
-	// Enable global interrupts
-	sei();
-
-
+	// main loop
     while(1) {
+    	// check for countdown reached 0
+    	if (frame_time == 0) {
+    		// reset the frame timer
+    		frame_time = current_frame_duration;
 
-		// every 1000ms toggle LED
-    	unsigned long now = millis();
-    	if (now - lastSecond > 1000) {
-    		lastSecond = now;
-			// Toggle Port B pin 4 output state
-			PORTB ^= 1<<PB4;
-		}
-
-
-    	if (now - prev_millis > 1000) {
-
-    		pattern_index++;
-    		if (pattern_index > 7) {
-    		    pattern_index = 0;
-    		}
+			if (current_letter == 1) {
+			  current_letter = 0;
+			} else {
+			  current_letter = 1;
+			}
+			setFrame(font[current_letter]);
     	}
 
-        //countTime();
-    	//cylon();
-    	//sandpit();
+    	if (time1 == 0) {
+    		// reset the multiplex timer
+    		time1 = T1;
+
+            outputRow(current_frame[current_row], current_frame[current_row], current_frame[current_row]);
+    	}
     }
 }
 
@@ -134,116 +118,106 @@ main (void)
 Functions
 ********************************************************************************/
 
-void sandpit()
+void initialize(void)
 {
+	// set up timer 0 for 1 mSec ticks (timer 0 is an 8 bit timer)
 
+	// Interupt mask register - to enable the interupt (datasheet: 14.9.6)
+	// (Bit 1 – OCIE0A: Timer/Counter0 Output Compare Match A Interrupt Enable)
+	TIMSK0 = (1 << OCIE0A); // (2) turn on timer 0 cmp match ISR
 
-	//draw();
+	// Compare register - when to interupt (datasheet: 14.9.4)
+	OCR0A = 249; // set the compare reg to 250 time ticks
 
-	//_delay_ms(1);
+    // Timer mode (datasheet: 14.9.1)
+	TCCR0A = (1 << WGM01); // (0b00000010) turn on clear-on-match
+
+	// Prescaler (datasheet: 14.9.2)
+	// calculation to show why 64 is required the prescaler:
+	// 1 / (16000000 / 64 / 256) = 0.001024 = 1ms (near enough)
+	TCCR0B = ((1 << CS10) | (1 << CS11)); // (0b00000011)(3) clock prescalar to 64
+
+	// Timer initialization
+    time1 = T1;
+    frame_time = current_frame_duration;
+
+	// crank up the ISRs
+	sei();
 }
 
-void draw() {
-
-	uint8_t i;
-	for (i = 0; i < 8; i++)  {
-		shiftRow(patterns[i]);
-		//_delay_ms(250);
-	}
-
-
-
-	/*
-	shiftRow(0b01111111); _delay_ms(250);
-	shiftRow(0b01111111); _delay_ms(250);
-	shiftRow(0b01111111); _delay_ms(250);
-	shiftRow(0b01111111); _delay_ms(250);
-
-	shiftRow(0b10111111); _delay_ms(250);
-	shiftRow(0b10111111); _delay_ms(250);
-    shiftRow(0b10111111); _delay_ms(250);
-    shiftRow(0b10111111); _delay_ms(250);
-    */
+void setFrame(uint8_t pattern[8])
+{
+  uint8_t i;
+  for (i = 0; i < 8; i++) {
+    current_frame[i] = pattern[i];
+  }
 }
 
-void shiftRow(char out) {
 
-
-	if (anodes == 0) {
-	    anodes = 1;
-	} else {
-
-	}
+void outputRow(uint8_t red, uint8_t blue, uint8_t green)
+{
 
 	// Latch LOW
 	PORTB &= ~(1 << PIN_LATCH);
 
-	shiftOut(LSBFIRST, out);
-    shiftOut(LSBFIRST, anodes);
+	// NOT the bytes so the patterns are 1 == ON 0 == OFF
+	shiftOut(MSBFIRST, ~(red));
+	shiftOut(MSBFIRST, ~blue);
+	// YEP, the hardware I built needs the green to be least significate bit first :(
+	shiftOut(LSBFIRST, ~green);
 
+	shiftOut(MSBFIRST, anodes);
+
+	// OE high
+    PORTC |= (1 << PIN_OE);
 	//take the latch pin high
 	PORTB |= (1 << PIN_LATCH);
+	// OE low
+	PORTC &= ~(1 << PIN_OE);
 
-	anodes <<= 1;
+	// At the end of the register add on bits to the start again.
+	if (anodes & (1 << 7)) {
+		// Shift left
+		anodes = (anodes << 1);
+		// Add one to the start (wrap around)
+		anodes |= (1 << 0);
+	} else {
+		// Shift left
+		anodes = (anodes << 1);
+	}
+
+	++current_row;
+	if (current_row > 7) {
+		current_row = 0;
+	}
+
 }
 
 void shiftOut(uint8_t bitOrder, uint8_t val)
 {
-        uint8_t i;
-        uint8_t bit;
+	uint8_t i;
+	uint8_t bit;
 
-        for (i = 0; i < 8; i++)  {
-        	if (bitOrder == LSBFIRST) {
-        		//digitalWrite(dataPin, !!(val & (1 << i)));
-        		bit = i;
-        	} else {
-        		//digitalWrite(dataPin, !!(val & (1 << (7 - i))));
-        		bit = 7 - i;
-        	}
+	for (i = 0; i < 8; i++)  {
+		if (bitOrder == LSBFIRST) {
+			//digitalWrite(dataPin, !!(val & (1 << i)));
+			bit = i;
+		} else {
+			//digitalWrite(dataPin, !!(val & (1 << (7 - i))));
+			bit = 7 - i;
+		}
 
-        	if (val & (1 << bit)) {
-        		PORTB |= (1 << PIN_DATA); // HIGH
-        	} else {
-        	    PORTB &= ~(1 << PIN_DATA); // LOW
-        	}
+		if (val & (1 << bit)) {
+			PORTB |= (1 << PIN_DATA); // HIGH
+		} else {
+			PORTB &= ~(1 << PIN_DATA); // LOW
+		}
 
-			//digitalWrite(clockPin, HIGH);
-        	PORTB |= (1 << PIN_CLOCK); // HIGH
+		//digitalWrite(clockPin, HIGH);
+		PORTB |= (1 << PIN_CLOCK); // HIGH
 
-			//digitalWrite(clockPin, LOW);
-        	PORTB &= ~(1 << PIN_CLOCK); // LOW
-        }
-}
-
-unsigned long millis()
-{
-	/*
-    unsigned long millis_return;
-    // Ensure this cannot be disrupted
-    ATOMIC_BLOCK(ATOMIC_FORCEON) {
-        millis_return = timer1_millis;
-    }
-    */
-    return timo_millis;
-}
-
-void countTime()
-{
-	for (int8_t x = 0; x <= 255; x++) {
-		// take the latchPin low so
-		// the LEDs don't change while you're sending in bits:
-		//digitalWrite(latchPin, LOW);
-		PORTB &= ~(1 << PIN_LATCH); // LOW
-
-		// shift out the bits:
-		shiftOut(LSBFIRST, x);
-
-		//take the latch pin high so the LEDs will light up:
-        //digitalWrite(latchPin, HIGH);
-		PORTB |= (1 << PIN_LATCH); // HIGH
-
-
+		//digitalWrite(clockPin, LOW);
+		PORTB &= ~(1 << PIN_CLOCK); // LOW
 	}
 }
-
 
