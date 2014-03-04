91ed1af6e411c5d6e29ebd3bd2329f3ab5343259
Peter Clarke <petermclarke@gmail.com>
2014-03-04 22:19:21 +0000

    Shift the data in the ISR every 0.0002ms

--- main.c
+++ main.c
@@ -13,7 +13,11 @@
 #define LSBFIRST 0
 #define MSBFIRST 1
 
-#define T1 2 // timeout value for the multiplex (mSec)
+#define COMPARE_REG 50 // OCR0A (1/5 of 1ms = 0.0002ms) - when to interupt (datasheet: 14.9.4)
+#define MILLIS_TICKS 5  // number of ISR calls before a millisecond is counted
+
+#define T1 2 * MILLIS_TICKS // timeout value (mSec)
+#define TICK_COUNTS 15 // Max number of ISR calls to count
 
 /********************************************************************************
 Function Prototypes
@@ -21,8 +25,8 @@ Function Prototypes
 
 void initialize(void); //all the usual mcu stuff
 void setFrame(uint8_t pattern[8]);
-void outputRow(uint8_t red, uint8_t blue, uint8_t green);
-void shiftOut(uint8_t bitOrder, uint8_t val);
+void shiftData();
+void shiftByte(uint8_t bitOrder, uint8_t val);
 
 
 
@@ -32,7 +36,7 @@ Global Variables
 uint8_t anodes = 0b11111110; // low = ON with pnp
 uint8_t current_row; // Which row of the frame is currently being shown via the multiplexing.
 uint8_t current_frame[8]; // The current frame being displayed
-unsigned long current_frame_duration = 2000; // millis to show the current frame.
+unsigned long current_frame_duration = 2000 * MILLIS_TICKS; // millis to show the current frame.
 unsigned long current_frame_start; // When the current frame was first shown.
 
 uint8_t current_letter; // tmp: the index of font to show
@@ -42,6 +46,7 @@ uint8_t font[2][8] = {
   {0x00,0x82,0xfe,0xfe,0x82,0x00,0x00,0x00}, // I
 };
 
+volatile uint8_t tick_count;
 volatile unsigned int time1;
 volatile unsigned int frame_time;
 
@@ -52,8 +57,6 @@ Interupt Routines
 //timer 0 compare ISR
 ISR (TIMER0_COMPA_vect)
 {
-	// Called every millis
-
 	// 75 cycles in
 	// 32 cycles save cpu state
 	// 8 cycles overhead
@@ -63,7 +66,10 @@ ISR (TIMER0_COMPA_vect)
 	// Decrement the time if not already zero
     if (time1 > 0)  --time1;
     if (frame_time > 0)  --frame_time;
+    if (tick_count > 0)  --tick_count;
 
+    // Shift out prepared data (red, blue, green, anodes) gets shifted out at this regular interval.
+    shiftData();
 }
 
 
@@ -77,17 +83,12 @@ main (void)
 	DDRC = 0xFF; // set all to output
 	PORTC |= (1 << PIN_OE); // Output enable high so the display is off
 
-
 	DDRB = 0xFF; // set all to output
 	PORTB = 0; // all off
 
-	initialize();
-
-
 	setFrame(font[current_letter]);
 
-	// Now enable the display by setting OE low.
-	PORTC &= ~(1 << PIN_OE);
+	initialize();
 
 	// main loop
     while(1) {
@@ -104,11 +105,15 @@ main (void)
 			setFrame(font[current_letter]);
     	}
 
+    	if (tick_count == 0) {
+    		tick_count = TICK_COUNTS;
+    	}
+
     	if (time1 == 0) {
-    		// reset the multiplex timer
+    		// reset the timer
     		time1 = T1;
 
-            outputRow(current_frame[current_row], current_frame[current_row], current_frame[current_row]);
+    		//shiftData();
     	}
     }
 }
@@ -127,19 +132,23 @@ void initialize(void)
 	TIMSK0 = (1 << OCIE0A); // (2) turn on timer 0 cmp match ISR
 
 	// Compare register - when to interupt (datasheet: 14.9.4)
-	OCR0A = 249; // set the compare reg to 250 time ticks
+	// OCR0A = 249; // set the compare reg to 250 time ticks = 1ms
+	//OCR0A = 124; // 0.5 ms
+	//OCR0A = 50; // 1/5 of 1ms = 0.0002ms
+	OCR0A = COMPARE_REG;
 
     // Timer mode (datasheet: 14.9.1)
 	TCCR0A = (1 << WGM01); // (0b00000010) turn on clear-on-match
 
 	// Prescaler (datasheet: 14.9.2)
 	// calculation to show why 64 is required the prescaler:
-	// 1 / (16000000 / 64 / 256) = 0.001024 = 1ms (near enough)
+	// 1 / (16000000 / 64 / 250) = 0.001 = 1ms
 	TCCR0B = ((1 << CS10) | (1 << CS11)); // (0b00000011)(3) clock prescalar to 64
 
 	// Timer initialization
     time1 = T1;
     frame_time = current_frame_duration;
+    tick_count = TICK_COUNTS;
 
 	// crank up the ISRs
 	sei();
@@ -153,28 +162,43 @@ void setFrame(uint8_t pattern[8])
   }
 }
 
-
-void outputRow(uint8_t red, uint8_t blue, uint8_t green)
+/**
+ * Shift out the prepared data.
+ */
+void shiftData()
 {
+	// Turn off the leds with OE high
+	// To prevent ghosting, while the data gets shifted.
+    PORTC |= (1 << PIN_OE);
 
-	// Latch LOW
+    // Latch LOW
+    // take the latchPin low so the LEDs don't change sending in bits:
 	PORTB &= ~(1 << PIN_LATCH);
 
 	// NOT the bytes so the patterns are 1 == ON 0 == OFF
-	shiftOut(MSBFIRST, ~(red));
-	shiftOut(MSBFIRST, ~blue);
+	//if (tick_count > 12) {
+	    shiftByte(MSBFIRST, ~current_frame[current_row]);
+	//} else {
+	//	shiftByte(MSBFIRST, ~0);
+	//}
+	shiftByte(MSBFIRST, ~current_frame[current_row]);
 	// YEP, the hardware I built needs the green to be least significate bit first :(
-	shiftOut(LSBFIRST, ~green);
+	//if (tick_count > 8) {
+	   shiftByte(LSBFIRST, ~current_frame[current_row]);
+    //} else {
+	//	shiftByte(MSBFIRST, ~0);
+	//}
 
-	shiftOut(MSBFIRST, anodes);
+	shiftByte(MSBFIRST, anodes);
 
-	// OE high
-    PORTC |= (1 << PIN_OE);
-	//take the latch pin high
+
+	// Take the latch pin high, to move the shifted data into position
 	PORTB |= (1 << PIN_LATCH);
-	// OE low
+
+	// OE low to turn on the leds
 	PORTC &= ~(1 << PIN_OE);
 
+	// Get the anodes ready for the next call
 	// At the end of the register add on bits to the start again.
 	if (anodes & (1 << 7)) {
 		// Shift left
@@ -193,7 +217,7 @@ void outputRow(uint8_t red, uint8_t blue, uint8_t green)
 
 }
 
-void shiftOut(uint8_t bitOrder, uint8_t val)
+void shiftByte(uint8_t bitOrder, uint8_t val)
 {
 	uint8_t i;
 	uint8_t bit;
@@ -213,10 +237,8 @@ void shiftOut(uint8_t bitOrder, uint8_t val)
 			PORTB &= ~(1 << PIN_DATA); // LOW
 		}
 
-		//digitalWrite(clockPin, HIGH);
+		// Clock in the bit
 		PORTB |= (1 << PIN_CLOCK); // HIGH
-
-		//digitalWrite(clockPin, LOW);
 		PORTB &= ~(1 << PIN_CLOCK); // LOW
 	}
 }
