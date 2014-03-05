21ef90349e0e679b16a308705ffdadf0b455a0c1
Peter Clarke <petermclarke@gmail.com>
2014-03-05 20:55:47 +0000

    Need to shift the data in the ISR to slow it down to prevent ghosting

--- main.c
+++ main.c
@@ -13,12 +13,13 @@
 #define LSBFIRST 0
 #define MSBFIRST 1
 
-#define COMPARE_REG 50 // OCR0A (1/5 of 1ms = 0.0002s) - when to interupt (datasheet: 14.9.4)
-#define MILLIS_TICKS 5  // number of ISR calls before a millisecond is counted
+// 32 * 0.000004 * 8 = 0.001024 = about 1khz (NB zero based so register one less)
+#define COMPARE_REG 31 // OCR0A when to interupt (datasheet: 14.9.4)
+#define MILLIS_TICKS 8  // number of ISR calls before a millisecond is counted (ish)
 
-#define T1 2 * MILLIS_TICKS // timeout value (mSec)
+#define T1 1 * MILLIS_TICKS // timeout value (mSec)
 #define COUNT_CYCLE_TICK 8 // number of ISR calls in a cycle
-#define COUNT_CYCLE 16 // number of cycles needed for 4bit bit angle modulation (0.0256s per cycle = 39hz ish)
+#define COUNT_CYCLE 16 // number of cycles needed for 4bit bit angle modulation
 
 /********************************************************************************
 Function Prototypes
@@ -34,11 +35,22 @@ void shiftByte(uint8_t bitOrder, uint8_t val);
 /********************************************************************************
 Global Variables
 ********************************************************************************/
-uint8_t data_shifted; // whether the data has been shifted and just needs to be latched.
-uint8_t cycle_count; // keeps track of the number of times a complete multiplex loop has happened.
-uint8_t anodes = 0b11111110; // low = ON with pnp
-uint8_t current_row; // Which row of the frame is currently being shown via the multiplexing.
-uint8_t current_frame[8]; // The current frame being displayed
+
+volatile uint8_t cycle_count; // keeps track of the number of times a complete multiplex loop has happened.
+//volatile uint8_t anodes = 0b11111110; // low = ON with pnp
+uint8_t anodes[8] = {
+  0b11111110,
+  0b11111101,
+  0b11111011,
+  0b11110111,
+  0b11101111,
+  0b11011111,
+  0b10111111,
+  0b01111111,
+};
+
+volatile uint8_t current_row; // Which row of the frame is currently being shown via the multiplexing.
+volatile uint8_t current_frame[8]; // The current frame being displayed
 unsigned long current_frame_duration = 2000 * MILLIS_TICKS; // millis to show the current frame.
 unsigned long current_frame_start; // When the current frame was first shown.
 
@@ -52,6 +64,7 @@ uint8_t font[2][8] = {
 volatile uint8_t cycle_tick_count;
 volatile unsigned int time1;
 volatile unsigned int frame_time;
+volatile uint8_t data_shifted; // whether the data has been shifted and just needs to be latched.
 
 /********************************************************************************
 Interupt Routines
@@ -72,8 +85,9 @@ ISR (TIMER0_COMPA_vect)
     if (cycle_tick_count > 0)  --cycle_tick_count;
 
     // Shift out prepared data (red, blue, green, anodes) gets shifted out at this regular interval.
-    //shiftData();
+    shiftData();
 
+/*
     // Data is (should be) ready to be latched
     // Turn off the leds with OE high
     PORTC |= (1 << PIN_OE);
@@ -85,6 +99,10 @@ ISR (TIMER0_COMPA_vect)
 
     // Output Enable low to turn on the leds
     PORTC &= ~(1 << PIN_OE);
+*/
+
+    // allow new data to be shifted
+    data_shifted = 0;
 }
 
 
@@ -105,8 +123,12 @@ main (void)
 
 	initialize();
 
+	PORTC &= ~(1 << PIN_OE); // leds on
+
 	// main loop
     while(1) {
+
+
     	// check for countdown reached 0
     	if (frame_time == 0) {
     		// reset the frame timer
@@ -138,11 +160,12 @@ main (void)
     		//shiftData();
     	}
 
-    	if (data_shifted == 0) {
+    	if (!data_shifted) {
     		// Shift the data ready to be latched in the ISR
-    		shiftData();
+    		//shiftData();
     		data_shifted = 1;
     	}
+
     }
 }
 
@@ -163,12 +186,15 @@ void initialize(void)
 	// OCR0A = 249; // set the compare reg to 250 time ticks = 1ms
 	//OCR0A = 124; // 0.5 ms
 	//OCR0A = 50; // 1/5 of 1ms = 0.0002s
+	//OCR0A = 30; // 0.00012s = 0.12ms
+	//OCR0A = 25; // 0.0001s = 0.1ms
 	OCR0A = COMPARE_REG;
 
     // Timer mode (datasheet: 14.9.1)
 	TCCR0A = (1 << WGM01); // (0b00000010) turn on clear-on-match
 
 	// Prescaler (datasheet: 14.9.2)
+	// 16MHz/64=250kHz so precision of 0.000004 = 4us
 	// calculation to show why 64 is required the prescaler:
 	// 1 / (16000000 / 64 / 250) = 0.001 = 1ms
 	TCCR0B = ((1 << CS10) | (1 << CS11)); // (0b00000011)(3) clock prescalar to 64
@@ -196,18 +222,22 @@ void setFrame(uint8_t pattern[8])
  */
 void shiftData()
 {
-	// Turn off the leds with OE high
-	// To prevent ghosting, while the data gets shifted.
-    //PORTC |= (1 << PIN_OE);
 
-    // Latch LOW
-    // take the latchPin low so the LEDs don't change sending in bits:
-	//PORTB &= ~(1 << PIN_LATCH);
+	// Latch LOW
+	PORTB &= ~(1 << PIN_LATCH);
+
+	// Turn off the leds with OE high
+	// To prevent ghosting/leakage.
+	// The process seems to need time to settle (parasitic capcitance?)
+	// The data shifting takes long enough for the settling to occur.
+	// This is why the the data is shifted in the ISR as well as the latching.
+    PORTC |= (1 << PIN_OE);
+
 
 	// NOT (invert) the bytes so the patterns are 1 == ON 0 == OFF
 
 	// pretend bit angle modulation - on for half the full number of 16 cycles
-	if (cycle_count > 8) {
+	if (cycle_count > 4) {
 	    shiftByte(MSBFIRST, ~current_frame[current_row]);
 	} else {
 		shiftByte(MSBFIRST, ~0);
@@ -217,35 +247,21 @@ void shiftData()
 	if (cycle_count > 8) {
 	   shiftByte(LSBFIRST, ~current_frame[current_row]);
     } else {
-		shiftByte(MSBFIRST, ~0);
+		shiftByte(LSBFIRST, ~0);
 	}
 
-	shiftByte(MSBFIRST, anodes);
-
-
-	// Take the latch pin high, to move the shifted data into position
-	//PORTB |= (1 << PIN_LATCH);
-
-	// OE low to turn on the leds
-	//PORTC &= ~(1 << PIN_OE);
-
-	// Get the anodes ready for the next call
-	// At the end of the register add on bits to the start again.
-	if (anodes & (1 << 7)) {
-		// Shift left
-		anodes = (anodes << 1);
-		// Add one to the start (wrap around)
-		anodes |= (1 << 0);
-	} else {
-		// Shift left
-		anodes = (anodes << 1);
-	}
+	shiftByte(MSBFIRST, anodes[current_row]);
 
 	++current_row;
 	if (current_row > 7) {
 		current_row = 0;
 	}
 
+    // Take the latch pin high, to move the shifted data into position
+	PORTB |= (1 << PIN_LATCH);
+
+	// OE low to turn on the leds
+	PORTC &= ~(1 << PIN_OE);
 }
 
 void shiftByte(uint8_t bitOrder, uint8_t val)
