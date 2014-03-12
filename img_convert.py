3052e5c4ae06c1f6d354a3d427be2890bcad56f8
Peter <petermclarke@gmail.com>
2014-03-12 17:33:37 +0000

    Experimental image conversion to 8x8 pixels

---
+++ img_convert.py
@@ -0,0 +1,31 @@
+import Image
+import sys
+
+# Or use imagemagick
+# convert -resize 8x8 -depth 3 smilie.jpg out.gif
+# or for raw rgb values:
+# convert -resize 8x8 -depth 3 smilie.jpg out.rgb
+
+# http://effbot.org/imagingbook/pil-index.htm
+# http://effbot.org/imagingbook/image.htm
+
+size = 8, 8
+
+im_orig = Image.open("in.jpg")
+im = im_orig.resize(size, Image.NEAREST)
+#im.convert(mode='P', dither=Image.NONE, colors=16, palette=Image.ADAPTIVE)
+im.convert("P", palette=Image.ADAPTIVE, colors=256)
+im.save("out.gif", "GIF")
+
+
+
+# pixels = list(im.getdata())
+# pixels = im.load()
+for x in range(8):
+    for y in range(8):
+        red, green, blue = im.getpixel((x,y))
+        buf = "%d,%d,%d" % (red, green, blue)
+	print buf
+        #cpixel = pixels[x, y]
+        #print cpixel
+
